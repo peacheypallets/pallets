9d8a304a951b3746ed214a97201ccea4e04bfdd0
Beebarnes <Beebarnes3@gmail.com>
2025-12-04 13:24:17 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import JobGrid from './features/JobTable/JobGrid.jsx';
+import Navbar from './components/Navbar.jsx';
+
 
 function App() {
+
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex flex-col justify-center content-between p-2 w-full">
+      <Navbar/>
+      <JobGrid/>
     </div>
   );
 }
