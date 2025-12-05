d50ceb38a04cdcea43c57e464d8e304a95d522dd
peacheypallets <peacheypallets@gmail.com>
2025-12-05 07:14:42 -0500

    Update config.js
    update to IP info

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,2 @@
-export const localIP = 'http://localhost:1337/api'
+export const localIP = 'https://informed-festival-846598c80b.strapiapp.com'
 // export const webIP = 'https://infinite-anchorage-30617.herokuapp.com'
